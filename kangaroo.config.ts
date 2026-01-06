091c280b962118d1ef04b4f071125e02a15c4f99
Eric Harris-Braun <eric@harris-braun.com>
2026-01-06 07:54:30 -0500

    Bump version to 0.1.1

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.lightningrodlabs.sharefeed',
   productName: 'ShareFeed',
-  version: '0.1.0',
+  version: '0.1.1',
   macOSCodeSigning: true,
   windowsEVCodeSigning: true,
   fallbackToIndexHtml: true,
